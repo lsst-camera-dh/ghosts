efd2b31745efeed03880dca84152d2a670e9d488
Johan Bregeon <johan.bregeon@gmail.com>
2022-01-25 12:48:23 +0100

    improve beam config description

--- ghosts/beam_configs.py
+++ ghosts/beam_configs.py
@@ -1,12 +1,14 @@
+from copy import copy
+
 # Default beam configs
-BEAM_CONFIG_0 = {'x_offset': 0., 'y_offset': 0,
-                 'radius': 0.00125,
-                 'wl': 500e-9, 'n_photons': 1000}
+BEAM_CONFIG_0 = {'id':0, 'wl': 500e-9, 'n_photons': 1000
+                 'x_offset': 0., 'y_offset': 0,'radius': 0.00125,
+                 'theta': 0., 'phi':0.}
 
-BEAM_CONFIG_1 = {'x_offset': 0.1, 'y_offset': 0,
-                 'radius': 0.00125,
-                 'wl': 500e-9, 'n_photons': 1000}
+BEAM_CONFIG_1 = copy(BEAM_CONFIG_0)
+BEAM_CONFIG_1['id'] = 1
+BEAM_CONFIG_1['x_offset'] = 0.1
 
-BEAM_CONFIG_2 = {'x_offset': 0., 'y_offset': 0.1,
-                 'radius': 0.00125,
-                 'wl': 500e-9, 'n_photons': 1000}
+BEAM_CONFIG_2 = copy(BEAM_CONFIG_0)
+BEAM_CONFIG_2['id'] = 2
+BEAM_CONFIG_2['y_offset'] = 0.1
