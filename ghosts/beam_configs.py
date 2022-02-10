677133e1cd7551beb14402df9b64d79c827d40ec
Johan Bregeon <bregeon@in2p3.fr>
2022-02-10 10:35:46 +0100

    lint beam_config

--- ghosts/beam_configs.py
+++ ghosts/beam_configs.py
@@ -1,9 +1,9 @@
 from copy import copy
 
 # Default beam configs
-BEAM_CONFIG_0 = {'id':0, 'wl': 500e-9, 'n_photons': 1000,
-                 'x_offset': 0., 'y_offset': 0,'radius': 0.00125,
-                 'theta': 0., 'phi':0.}
+BEAM_CONFIG_0 = {'id': 0, 'wl': 500e-9, 'n_photons': 1000,
+                 'x_offset': 0., 'y_offset': 0, 'radius': 0.00125,
+                 'theta': 0., 'phi': 0.}
 
 BEAM_CONFIG_1 = copy(BEAM_CONFIG_0)
 BEAM_CONFIG_1['id'] = 1
