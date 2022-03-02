2a42ffd6a044bb68d6ab1ea0299d8dc8ee1c4450
Johan Bregeon <johan.bregeon@gmail.com>
2022-03-02 12:13:18 +0100

    new helpers to build sets of geometries

--- ghosts/geom.py
+++ ghosts/geom.py
@@ -6,7 +6,10 @@ Todo:
     *
 
 """
+import copy
 import pandas as pd
+from ghosts.tools import get_vector
+from ghosts.geom_config import GEOM_CONFIG_0
 
 
 def to_panda(geom_config):
@@ -84,3 +87,104 @@ def concat_dicts(geom_dict_list):
         frames.append(to_panda(one))
     geom_concat = concat_frames(frames)
     return geom_concat
+
+
+# Helpers to create a set of geometries translations
+def translate_optic(optic_name, axis, distance, geom_id=1000000):
+    """ Create a dictionary to translate a piece of optic along an axis
+
+     Parameters
+     ----------
+    optic_name : `string`
+        the name of an optical element
+    axis : `string`
+        the name of the translation axis, usually y
+    distance : `float`
+        the value of the shift in meters
+    geom_id : `int`
+        the id of the new geometry configuration
+
+     Returns
+     -------
+     geom : `dict`
+        a `geom_config` dictionary for the application of the translation
+     """
+    geom = copy.deepcopy(GEOM_CONFIG_0)
+    geom['geom_id'] = geom_id
+    geom[optic_name]['shifts'] = get_vector(axis, distance)
+    return geom
+
+
+def rotate_optic(optic_name, axis, angle, geom_id=1000000):
+    """ Rotate one optical element of a telescope given a list of Euler angles
+
+    Parameters
+    ----------
+    optic_name : `string`
+        the name of an optical element
+    axis : `string`
+        the name of the rotation axis, usually y
+    angle : `float`
+        the values of angle in degrees
+    geom_id : `int`
+        the id of the new geometry configuration
+
+    Returns
+    -------
+     geom : `dict`
+        a `geom_config` dictionary for the application of the rotation
+    """
+    geom = copy.deepcopy(GEOM_CONFIG_0)
+    geom['geom_id'] = geom_id
+    geom[optic_name]['rotations'] = get_vector(axis, angle)
+    return geom
+
+
+def build_translation_set(optic_name, axis, shifts_list, base_id=0):
+    """ Build a set of geometries for the given list of translations
+
+    Parameters
+    ----------
+    optic_name : `string`
+        the name of an optical element
+    axis : `string`
+        the name of the rotation axis, usually y
+    shifts_list : `list` of `float`
+        the list of distances to scan in meters
+    base_id : `int`
+        the id of the first geometry configuration created, following ids will be `id+1`
+
+    Returns
+    -------
+     geoms : `list` of `geom_config`
+        a list of geometry configuration dictionaries
+    """
+    geoms = list()
+    for i, shift in enumerate(shifts_list):
+        geoms.append(translate_optic(optic_name, axis, shift, geom_id=base_id+i))
+    return geoms
+
+
+def build_rotation_set(optic_name, axis, angles_list, base_id=0):
+    """ Build a set of geometries for the given list of rotations
+
+    Parameters
+    ----------
+    optic_name : `string`
+        the name of an optical element
+    axis : `string`
+        the name of the rotation axis, usually y
+    angles_list : `list` of `float`
+        the list of angles to scan in degrees
+    base_id : `int`
+        the id of the first geometry configuration created, following ids will be `id+1`
+
+    Returns
+    -------
+     geoms : `list` of `geom_config`
+        a list of geometry configuration dictionaries
+    """
+    geoms = list()
+    for i, angle in enumerate(angles_list):
+        geoms.append(rotate_optic(optic_name, axis, angle, geom_id=base_id+i))
+    return geoms
