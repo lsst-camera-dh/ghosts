95557586a33059b1f0a42ce660df928a2acba0f0
Johan Bregeon <johan.bregeon@gmail.com>
2022-03-02 12:12:39 +0100

    add numpy in requirements

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         ],
     install_requires=["batoid",
                       "matplotlib",
+                      "numpy",
                       "scipy",
                       "pandas",
                       "openpyxl",
