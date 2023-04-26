63866e31d74764f54dce31e7cb4eaf6fcd777087
Johan Bregeon <bregeon@in2p3.fr>
2023-04-26 11:43:08 +0200

    read beam set from parquet file

--- scripts/run_full_fit.py
+++ scripts/run_full_fit.py
@@ -1,8 +1,12 @@
 """ A script to run a full fit of the geometry, using the GhostFitter class
 """
 import logging
+
+import pandas as pd
+
 from ghosts.ghosts_fit import GhostsFitter
 from ghosts.beam_configs import BASE_BEAM_SET
+from ghosts.beam import to_beam_set
 
 
 if __name__ == '__main__':
@@ -34,8 +38,15 @@ if __name__ == '__main__':
     # Set logging level
     logging.basicConfig(level=logging.DEBUG)
 
+    # Read from file and select beam configs for the fit
+    beam_frame = pd.read_parquet("./data/ghosts_reference_beam_configurations.parquet")
+    sub_bf = beam_frame[(beam_frame['beam_id'] > 999010) & (beam_frame['beam_id'] < 999064)]
+    ghost_beam_set = to_beam_set(sub_bf)
+    logging.info('%d beam configurations will be used for the fit.' % len(ghost_beam_set))
+
     # Run!
-    fitter = GhostsFitter(yaml_geom="./data/LSST_CCOB_r_aligned.yaml", beam_set=BASE_BEAM_SET)
+    fitter = GhostsFitter(yaml_geom="./data/LSST_CCOB_r_aligned.yaml", beam_set=ghost_beam_set)
+    # fitter = GhostsFitter(yaml_geom="./data/LSST_CCOB_r_aligned.yaml", beam_set=BASE_BEAM_SET)
     fitter.setup_reference_ghosts()
     fitter.run(mode=args.mode, n_calls=int(args.n_calls), precision=float(args.precision),
                with_cov=True, optics_list=['L1', 'L2', 'L3'], n_sub_calls=50)
